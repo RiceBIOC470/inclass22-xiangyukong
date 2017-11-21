52880467e0e28b26fe91cc953dafa65aab189649
xiangyukong <31257615+xiangyukong@users.noreply.github.com>
2017-11-21 14:16:14 -0600

    Add files via upload

--- Inclass22.m
+++ Inclass22.m
@@ -6,5 +6,61 @@
 %transcripton factor are necessary to activate transciption. Comment on your
 %results. 
 
+figure; hold on;
+ku = 0.1
+for kb = 0:0.05:8;
+    polycoeff = [1 -kb 1 -ku];
+    rts = roots(polycoeff);
+    rts = rts(imag(rts)==0);
+    plot(kb*ones(length(rts),1),rts,'r.');
+end
+hold off;
+xlabel('k_b');
+ylabel('fixed point');
+set(gca,'FontSize',22)
+%in this case when the binding rate of the factor is higher than a certain
+%number or lower than another, the regulation can not be initiated.
+
+figure; hold on;
+ku = 0.1
+for kb = 0:0.05:8;
+    polycoeff = [1 -kb 0 0 1 -ku];
+    rts = roots(polycoeff);
+    rts = rts(imag(rts)==0);
+    plot(kb*ones(length(rts),1),rts,'r.');
+end
+hold off;
+xlabel('k_b');
+ylabel('fixed point');
+set(gca,'FontSize',22)
+
+figure; hold on;
+ku = 0.1
+for kb = 0:0.05:8;
+    polycoeff = [1 -kb 0 0 0 0 0 0 1 -ku];
+    rts = roots(polycoeff);
+    rts = rts(imag(rts)==0);
+    plot(kb*ones(length(rts),1),rts,'r.');
+end
+hold off;
+xlabel('k_b');
+ylabel('fixed point');
+set(gca,'FontSize',22)
+%for these two condition there is only a lower condition for the regulation
+%to be initiated properly.
+
 % 2. Make a similar diagram for the case of an autorepressing gene in the
 % case that 2 copies are need to turn off the gene. 
+
+figure; hold on;
+ku = 2
+for kb = 0:0.05:8;
+    polycoeff = [1 -kb 1 -ku];
+    rts = roots(polycoeff);
+    rts = rts(imag(rts)==0);
+    plot(kb*ones(length(rts),1),rts,'r.');
+end
+hold off;
+xlabel('k_b');
+ylabel('fixed point');
+set(gca,'FontSize',22)
