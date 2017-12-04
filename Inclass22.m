f3b9f6a7f4c97b2328d24e80bced36093eed947d
gbri140 <glb3@rice.edu>
2017-12-03 22:09:54 -0600

    Update Inclass22.m

--- Inclass22.m
+++ Inclass22.m
@@ -1,4 +1,8 @@
 %Inclass 22
+%GB comments
+1 100
+2 100
+overall 100
 
 %1. Consider the case of the auto-activating gene that we discussed in class
 %today. Make a bifurcation diagram for this system by varying the
